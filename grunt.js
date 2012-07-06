cba8e8567828ab0443ef70b0cbb2f3350c86d480
David Hong <davidhong.code@gmail.com>
2012-07-06 12:08:55 +1000

    Adding concat task to Grunt.js

--- grunt.js
+++ grunt.js
@@ -1,9 +1,14 @@
 module.exports = function (grunt) {
 
 	grunt.initConfig({
+		pkg: '<json:package.json>',
+
+		/* Test, test, test */
 		test: {
 			file: ['test/**/*.js']
 		},
+
+		/* Code linter */
 		lint: {
 			all: ['src/**/*.js', 'test/**/*.js']
 		},
@@ -12,6 +17,15 @@ module.exports = function (grunt) {
 				white:  true,
 				es5:    true,
 				node:   false
+			}
+		},
+
+		/* Distribution */
+		concat: {
+			dist: {
+				src: ['lib/RFC5988-parser.js', 'src/Backbone.LinkedCollection.js'],
+				dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js',
+				separated: ';'
 			}
 		}
 	});
